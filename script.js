4e08a599a41a11f1781613ca45221535da721dfe
Syeda Nahal Mansoor <nahalsyeda@gmail.com>
2026-01-18 01:51:34 +0500

    added faqs

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
         mobileMenu.classList.toggle("active");
     });
 });
+
+const faqQuestions = document.querySelectorAll(".faq-question");
+
+faqQuestions.forEach(question => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+    const icon = question.querySelector("span");
+
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+      icon.textContent = "+";
+    } else {
+      document.querySelectorAll(".faq-answer").forEach(a => a.style.maxHeight = null);
+      document.querySelectorAll(".faq-question span").forEach(i => i.textContent = "+");
+
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      icon.textContent = "−";
+    }
+  });
+});
